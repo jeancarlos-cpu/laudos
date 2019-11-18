9fd7424072904c4f3896c52ba63e0dd2d324b9be
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-11-18 14:42:01 -0300

    Updates

---
+++ precache-manifest.cf8c7680eed2ef12216fb147c00faa19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3ce7c38d794223bd0e3f145a801409",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "78677171f08b5ac3a976",
+    "url": "/ecommerce/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3e3b7983c43df8562b71",
+    "url": "/ecommerce/static/js/2.5c0d84cb.chunk.js"
+  },
+  {
+    "revision": "78677171f08b5ac3a976",
+    "url": "/ecommerce/static/js/main.7c4fd921.chunk.js"
+  },
+  {
+    "revision": "3ede6eaee99b50c0cd49",
+    "url": "/ecommerce/static/js/runtime-main.97ac5629.js"
+  }
+]);
